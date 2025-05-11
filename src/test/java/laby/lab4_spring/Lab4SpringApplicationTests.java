b724f532e227918740966b453a6b9848b19fa293
Hajaro <Hajaro@users.noreply.github.com>
2025-05-11 10:06:32 +0200

    init

---
+++ src/test/java/laby/lab4_spring/Lab4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package laby.lab4_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
